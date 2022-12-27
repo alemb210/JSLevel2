571d05d3737c4f9600e8886e01bb9be2d5b2394d
alemb210 <99274936+alemb210@users.noreply.github.com>
2022-12-27 18:24:40 -0500

    Add files via upload

--- MatchGame/index.js
+++ MatchGame/index.js
@@ -5,49 +5,52 @@ var lastClass = "";
 var awaiting = false;
 var score = 0;
 
-console.log(cards);
+/*console.log(cards); */
 function flipCard(id, type){
-    console.log("awaiting = " + awaiting);
+    /*console.log("awaiting = " + awaiting); */
     if(awaiting) {
-        console.log("You cannot flip a card right now!");
+        alert("You cannot flip a card right now!"); 
         return;
     }
-    console.log("START OF FUNCTION " + "lastCard = " + lastCard + " lastClass = " + lastClass + " clickActive = " + clickActive);
+    /*console.log("START OF FUNCTION " + "lastCard = " + lastCard + " lastClass = " + lastClass + " clickActive = " + clickActive);*/
     console.log("card flipped with id " + id + " with class " + type);
     if(document.getElementById(id).style.backgroundColor == "") {
         document.getElementById(id).style.backgroundColor = type;
-    }
-    else {
-        document.getElementById(id).style.backgroundColor = "";
+        document.getElementById(id).innerHTML = "";
     }
     console.log("Checking if match " + "clickActive = " + clickActive + " type = " + type + " lastCard = " + lastCard + " lastClass = " + lastClass); 
     if(clickActive) {
         if(type === lastClass) {
-            console.log("Match!!")
+            /*console.log("Match!!")*/
             document.getElementById(id).style.backgroundColor = type;
+            document.getElementById(id).innerHTML = "";
             document.getElementById(lastCard).style.backgroundColor = lastClass;
+            document.getElementById(lastCard).innerHTML = "";
         }
         else { 
             document.getElementById(id).style.backgroundColor = type;
+            document.getElementById(id).innerHTML = "";
             awaiting = true;
             console.log("Waiting... awaiting = " + awaiting);
             setTimeout(() => {
                 document.getElementById(id).style.backgroundColor = "";
+                document.getElementById(id).innerHTML = "?";
                 document.getElementById(lastCard).style.backgroundColor = "";
+                document.getElementById(lastCard).innerHTML = "?";
                 clearMemory();
                 awaiting = false;
                 console.log("Waiting complete! awaiting = " + awaiting);
             }, 2000)
         }
     }
-    console.log("Before inversion, clickActive = " + clickActive);
+    /*console.log("Before inversion, clickActive = " + clickActive);*/
     clickActive = !clickActive;
-    console.log("Checking to store last card if true, clickActive = " + clickActive);
+    /*console.log("Checking to store last card if true, clickActive = " + clickActive);*/
     if(clickActive) {
         lastCard = id;
         lastClass = type;
     }
-    console.log("END OF FUNCTION " + "lastCard = " + lastCard + " lastClass = " + lastClass + " clickActive = " + clickActive);
+    /*console.log("END OF FUNCTION " + "lastCard = " + lastCard + " lastClass = " + lastClass + " clickActive = " + clickActive);*/
     score++;
     update();
 }
@@ -59,30 +62,49 @@ function clearMemory() {
 
 function shuffle() { 
     console.log("Shuffling...")
-    for(let i = 1; i <= cards.length; i++) {
+    for(let i = 0; i < cards.length; i++) {
         let random = Math.floor(Math.random() * cards.length);
-        let tempCard = "card" + i;
-        console.log("i = " + i + "random = " + random + "tempCard = " + tempCard);
-        document.getElementById(tempCard).style.order = random;
+        /*console.log("i = " + i + "random = " + random + "currentCard = " + cards.item(i));*/
+        cards.item(i).style.order = random;
+        cards.item(i).innerHTML = "?";
     }
 }
 
 function update() { 
-    document.getElementById("score").innerHTML = "Score: " + score;
-}
-/* class Card {
-    constructor(type, position) {
-    this.type = type;
-    this.position = position;
+    document.getElementById("score").innerHTML = "Score: " + score;   
+    let endCheck = true;
+    for(let i = 0; i < cards.length; i++){
+        console.log(cards.item(i).className.split(' ')[1]);
+        console.log(cards.item(i).style.backgroundColor);
+        if(cards.item(i).className.split(' ')[1] != cards.item(i).style.backgroundColor) {
+            endCheck = false;
+        }
     }
-
+    if(endCheck) {
+        if(score === 16) {
+            alert("Perfect Score!! Your score was " + score);
+        }
+        else if(score > 16 && score < 28) {
+            alert("Great score! Your score was " + score);
+        }
+        else if(score > 28 && score < 40) {
+            alert("Good score, your score was " + score);
+        }
+        else if(score > 40 && score < 60) {
+            alert("OK score. Try to do better next time.") 
+        }
+        else if(score > 60) {
+            alert("Bad score..")
+        }
+    }
 }
 
-function initialize {
-    cards.length = 0;
-    for(let i = 0; i < 16; i++) {
-        let randType = Math.floor(Math.random() * 8);
-        cards.push(new Card(randType, i));
-        console.log(cards);
+function reset() {
+    for(let i = 0; i < cards.length; i++){
+        cards.item(i).style.backgroundColor = "";
     }
-} */ 
+    score = 0;
+    update();
+    shuffle();
+}
+
